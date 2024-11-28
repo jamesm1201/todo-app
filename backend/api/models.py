cbd156d0d3fa3910bef43aab011aabf4c38fad7d
James <jhmorley@icloud.com>
2024-11-28 20:16:40 +0000

    create note model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Note model contains fields for the todo note
+#created_at automatically records the datetime it was made
+#Foreign key links to user model, related name allows you to find all a user's related notes
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
 
+    def __str__(self):
+        return self.title
 
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Note
 #This takes in JSON data and converts to Python equivalent and vice versa
 
 class UserSerializer(serializers.Serializer):
@@ -13,3 +13,9 @@ class UserSerializer(serializers.Serializer):
         #** splits arguments and passes from a dictionary
         user = User.objects.create_user(**validated_data)
         return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notefields = ["id", "title", "content", "created_at", "author"]
+        #Ensures authenticated user is the only author and cannot change
+        extra_kwargs = {"author": {"read-only": True}}
